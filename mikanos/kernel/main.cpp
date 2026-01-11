27e1988c1c0c5981f79ea022c0c359c7dca616d3
pirotyyy <hiroki1423@docomo.ne.jp>
2026-01-11 02:39:21 +0000

    feat: implement printk

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -2,10 +2,10 @@
 #include <cstdint>
 #include <cstdio>
 
+#include "console.hpp"
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
-#include "console.hpp"
 
 void* operator new(size_t size, void* buf) {
   return buf;
@@ -15,6 +15,22 @@ void operator delete(void* obj) noexcept {}
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -34,12 +50,11 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  console =
+      new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
   for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (1)
